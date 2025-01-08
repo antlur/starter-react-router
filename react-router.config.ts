ff4b4f1610ded54ad6acf56a4a4113bb4925af7c
Anthony Holmes <anthony@antlur.co>
2025-01-08 00:21:30 -0600

    prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  // async prerender() {
-  //   return ["/"];
-  // },
+  prerender: true,
 } satisfies Config;
