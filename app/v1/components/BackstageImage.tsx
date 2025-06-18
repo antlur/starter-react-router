69d99e52141e8ab1053dcd5c8c17b4fa7f814f41
Anthony Holmes <anthony@antlur.co>
2025-06-18 03:36:34 -0500

    feat: add BackstageImage component for dynamic image rendering

---
+++ app/v1/components/BackstageImage.tsx
@@ -0,0 +1,29 @@
+"use client";
+// import { Image } from "@/components/image";
+import { cn } from "@/v1/lib/utils";
+import { Image, type ImageProps } from "@unpic/react";
+import { env } from "@/lib/env";
+
+interface BackstageImageProps extends ImageProps {
+  image: any;
+  // maxWidth?: number;
+  // src?: string;
+  // loading?: "lazy" | "eager";
+  // width?: number;
+  // height?: number;
+  // className?: string;
+  // alt?: string;
+}
+
+export default function BackstageImage({ image, src, alt, loading = "lazy", ...props }: ImageProps) {
+  return (
+    <Image
+      src={(src || image?.url) ?? ""}
+      alt={(alt || image?.alt) ?? ""}
+      width={props.width ?? image?.width}
+      height={props.height ?? image?.height}
+      className={cn("object-cover w-full h-full", props.className)}
+      cdn={env("NETLIFY") ? "netlify" : undefined}
+    />
+  );
+}
